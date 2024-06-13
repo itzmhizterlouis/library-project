3a855e1545d78b9360182d07abd6dda8a8443b2d
Marvellous <itzmhizterlouis@gmail.com>
2024-06-13 01:14:27 +0100

    Endpoint for finding all users and something tofinish last

--- src/main/java/com/hslcreator/LibraryAPI/controllers/UserController.java
+++ src/main/java/com/hslcreator/LibraryAPI/controllers/UserController.java
@@ -1,6 +1,7 @@
 package com.hslcreator.LibraryAPI.controllers;
 
 
+import com.hslcreator.LibraryAPI.exceptions.UnauthorizedException;
 import com.hslcreator.LibraryAPI.exceptions.UserNotFoundException;
 import com.hslcreator.LibraryAPI.models.responses.UserResponse;
 import com.hslcreator.LibraryAPI.repositories.UserRepository;
@@ -9,18 +10,46 @@ import com.hslcreator.LibraryAPI.utils.UserUtil;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("users/")
 public class UserController {
 
+    private final UserService userService;
+
     @Operation(summary = "Get Logged in user", description = "Get user details for logged in user")
     @GetMapping("me")
     public UserResponse getLoggedInUser() {
 
         return UserUtil.getLoggedInUser().orElseThrow(UserNotFoundException::new).toDto();
+    }
+
+    @Operation(summary = "Get All Users")
+    @PostMapping("all")
+    public List<UserResponse> findAllUsers() throws UnauthorizedException {
+
+        return userService.findAllUsers();
+    }
+
+    @Operation(summary = "Suspend Account")
+    @PutMapping("account/suspend/{userId}")
+    public UserResponse suspendAccount(@PathVariable int userId) throws UnauthorizedException {
+
+        return userService.suspendAccount(userId);
+    }
+
+    @Operation(summary = "Un-Suspend Account")
+    @PutMapping("account/unsuspend/{userId}")
+    public UserResponse unSuspendAccount(@PathVariable int userId) throws UnauthorizedException {
+
+        return userService.unSuspendAccount(userId);
     }
 }
--- src/main/java/com/hslcreator/LibraryAPI/services/UserService.java
+++ src/main/java/com/hslcreator/LibraryAPI/services/UserService.java
@@ -1,12 +1,16 @@
 package com.hslcreator.LibraryAPI.services;
 
 
+import com.hslcreator.LibraryAPI.exceptions.UnauthorizedException;
 import com.hslcreator.LibraryAPI.exceptions.UserNotFoundException;
 import com.hslcreator.LibraryAPI.models.entities.User;
+import com.hslcreator.LibraryAPI.models.responses.UserResponse;
 import com.hslcreator.LibraryAPI.repositories.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @AllArgsConstructor
@@ -22,5 +26,31 @@ public class UserService {
     public User findUserById(int id) throws UserNotFoundException {
 
         return userRepository.findByUserId(id).orElseThrow(UserNotFoundException::new);
+    }
+
+    public List<UserResponse> findAllUsers() throws UnauthorizedException {
+
+        LibraryService.throwErrorIfUserNotAdmin();
+        return userRepository.findAll().stream().map(User::toDto).toList();
+    }
+
+    public UserResponse suspendAccount(int userId) throws UnauthorizedException {
+
+        LibraryService.throwErrorIfUserNotAdmin();
+
+        User user = userRepository.findByUserId(userId).orElseThrow(UserNotFoundException::new);
+        user.setLocked(true);
+        return userRepository.save(user).toDto();
+    }
+
+    public UserResponse unSuspendAccount(int userId) throws UnauthorizedException {
+
+        // TODO: to finish this and the former before tomorrow
+
+        LibraryService.throwErrorIfUserNotAdmin();
+
+        User user = userRepository.findByUserId(userId).orElseThrow(UserNotFoundException::new);
+        user.setLocked(false);
+        return userRepository.save(user).toDto();
     }
 }
