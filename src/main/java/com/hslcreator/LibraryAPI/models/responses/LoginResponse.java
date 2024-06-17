d88aec9d0d353a7e0f884de93519dc59868c2dd3
Marvellous <itzmhizterlouis@gmail.com>
2024-06-17 07:59:52 +0100

    added department to the response

--- src/main/java/com/hslcreator/LibraryAPI/models/responses/LoginResponse.java
+++ src/main/java/com/hslcreator/LibraryAPI/models/responses/LoginResponse.java
@@ -1,5 +1,6 @@
 package com.hslcreator.LibraryAPI.models.responses;
 
+import com.hslcreator.LibraryAPI.models.entities.Department;
 import com.hslcreator.LibraryAPI.models.entities.Role;
 import lombok.Builder;
 import lombok.Data;
@@ -11,4 +12,5 @@ public class LoginResponse {
     private int userId;
     private String token;
     private Role role;
+    private Department department;
 }
